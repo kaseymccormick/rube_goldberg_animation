435af1813f46ec7439289322aaa68436dc4008e9
Kasey McCormick <mccormick.kasey@gmail.com>
2015-08-20 23:44:37 -0500

    5 animations done 15 to go

---
+++ dev/README.md
@@ -0,0 +1,22 @@
+Rube Goldberg Machine
+
+-turn the faucet handle-
+-to start the water-
+to fill up the pot
+-while the burner turns on-
+to make steam
+that collects in the parachute
+-the lifts to the bookcase-
+and knocks the gear
+that hits the books
+that make the bowling pin wobble 
+that kocks off the magnet
+that lifts the bag with jacks and bouncy ball
+which lowers the magnet
+that attracts the metal pole in the rolly chair
+which makes the chair move
+that makes the dolly rock
+and tip over
+making the box tip on it's side
+so the open flap allows the mouse cursor to escape
+so the mouse cursor finds it's way to the "find out more button"
--- dev/js/site/app.js
+++ dev/js/site/app.js
@@ -1,36 +1,34 @@
-window.onload = setupDom;
+window.onload = function (){
 
-function setupDom(){
- 
-  
-  var graphic = document.getElementsByClassName('the_machine')[0];
-  
-  var graphicDoc = graphic.contentDocument;
-  
-  // get the object for greensock to target
-  var fHandle = graphicDoc.getElementById("faucet_x5F_handle");
-  var droplet = graphicDoc.getElementById("droplet");
-  
-  var burner = graphicDoc.getElementById("ovenburner");
-  var steam = graphicDoc.getElementById("");
-  
-  var parachute = graphicDoc.getElementById("parachute");
-  
-    console.log(burner)
+
+var graphic = document.getElementsByClassName('the_machine')[0];
+    graphicDoc = graphic.contentDocument;
+
+    // get the object for greensock to target
+    fHandle = graphicDoc.getElementById("faucet_x5F_handle");
+    droplet = graphicDoc.getElementById("droplet");
+
+    burner = graphicDoc.getElementById("ovenburner");
+    steam = graphicDoc.getElementById("steam_x5F_cloud");
+
+    parachute = graphicDoc.getElementById("parachute");
+    bsGear = graphicDoc.getElementById("bsgear")
   
   //------------------------------------------
   //    Animations
   //--------------------------------------------
   //handle and water droplet change repeats to -1
-  TweenMax.to(fHandle, 5, { repeat: 10, rotationY: 360, transformOrigin: "50% 50%", onComplete: console.log("Handle moved") }); 
-  TweenMax.to(droplet, 1.5, { delay:2, y: 320, x:20, ease: Power1.easeInOut, repeat: 10, onComplete: console.log("complete") });
-  TweenMax.to(droplet, 3, {fill:"blue"})
-  //burner and steam
-  // TweenMax.to(burner,1,{opacity:1});
+  TweenMax.to(fHandle, 5, { repeat: 10, rotationY: 360, transformOrigin: "50% 50%", onComplete: console.log("Handle moved") });
+  TweenMax.to(droplet, 1.5, { delay:5, y: 320, x:20, ease: Power1.easeInOut, repeat: 10, fill:"blue", onComplete: console.log("complete") });
   
-  //parachute
-  TweenMax.to(parachute, 10, { y: -220, x:260, ease: Power1.easeInOut, repeat: 10, onComplete: console.log("complete") });
-  
+ //burner and steam
+  TweenMax.to(burner, 3, {delay:6,repeat:3, fill:"red", yoyo: true, onComplete: startParachute});
+}
+function startParachute(){
+  // //parachute\    delay:15,
+  TweenMax.to(parachute, 10, {  y: -208, x:245, ease: Power1.easeInOut, onComplete: startGear });
+ }  
+  // TweenMax.from(bsGear, 5, {rotation:-360, transformOrigin:"50% 50%", x:60});
   
   //            variable, time, {what to do}
   // arm animation
@@ -39,4 +37,13 @@ function setupDom(){
   // TweenMax.from(wheel, 1, {rotation:-30, scale:1, transformOrigin:"200px 200px"});
  //  // car animation
  //  TweenMax.from(car, 1, {x: 200});
+
+function startGear(){
+  // var graphic = document.getElementsByClassName('the_machine')[0];
+  //
+  // var graphicDoc = graphic.contentDocument;
+  // var bsGear = graphicDoc.getElementById("bsgear")
+ TweenMax.to(bsGear, 2, {rotation:360, transformOrigin:"50% 50%", x: 100, ease: Power0.easeNone});
+  
+  
 }
