5160b1800fff189f25ae0d02a9bae3ad083428e2
Kasey McCormick <mccormick.kasey@gmail.com>
2015-08-24 15:03:17 -0500

    steam animation

--- dev/js/site/app.js
+++ dev/js/site/app.js
@@ -9,7 +9,7 @@ var graphic = document.getElementsByClassName('the_machine')[0];
     droplet = graphicDoc.getElementById("droplet");
 
     burner = graphicDoc.getElementById("ovenburner");
-    steam = graphicDoc.getElementById("steam_x5F_cloud");
+    steam = graphicDoc.getElementById("steam_cloud");
 
     parachute = graphicDoc.getElementById("parachute");
     bsGear = graphicDoc.getElementById("bsgear")
@@ -33,34 +33,32 @@ var graphic = document.getElementsByClassName('the_machine')[0];
   //    Animations
   //--------------------------------------------
   //handle and water droplet change repeats to -1
-    TweenLite.to(bag, 1, {y:-440});
-    TweenLite.to(hook, 1, {y:-440});
 
     
-  TweenMax.to(fHandle, 5, { repeat: 10, rotationY: 360, transformOrigin: "50% 50%", onComplete: console.log("Handle moved") });
+  TweenMax.to(fHandle, 5, { onStart: startSteam, repeat: 10, rotationY: 360, transformOrigin: "50% 50%", onComplete: console.log("Handle moved") });
   TweenMax.to(droplet, 1.5, { delay:5, y: 320, x:20, ease: Power1.easeInOut, repeat: 10, fill:"blue", onComplete: console.log("complete") });
 
   
   
   TweenMax.to(magnet, 1, {repeat:10, x:5})
  //burner and steam
-  TweenMax.to(burner, 3, {delay:6,repeat:3, fill:"red", yoyo: true, onComplete: startParachute});
+  TweenMax.to(burner, 3, {delay:6,repeat:3, fill:"red", yoyo: true, onComplete: startSteam});
   
   TweenMax.to(bowlingPin, 1,{rotation:3, transoformOrigin: "100% 100%"});
+}
+function startSteam(){
+     
+      var tl = new TimelineMax({repeat: 5});
+
+      tl.to(steam, 3, { y:-100, x:60, ease: Power1.easeInOut})
+      .to(steam, 1, {scale: 1.2, ease: Back.easeOut.config(1.7)})
+      // .to(steam,.3, {scale: 1, ease: Power0.easeNone});
+
 }
 function startParachute(){
   // //parachute\    delay:15,
   TweenMax.to(parachute, 10, {  y: -208, x:245, ease: Power1.easeInOut, onComplete: startGear });
  }  
-  // TweenMax.from(bsGear, 5, {rotation:-360, transformOrigin:"50% 50%", x:60});
-  
-  //            variable, time, {what to do}
-  // arm animation
-  // TweenMax.from(wheel, .7, {transformOrigin: '100% 100%', rotation: 45});
-  // TweenMax.to(wheel, .5, {transformOrigin: '50% 50%', scale:1.2, delay:.9});
-  // TweenMax.from(wheel, 1, {rotation:-30, scale:1, transformOrigin:"200px 200px"});
- //  // car animation
- //  TweenMax.from(car, 1, {x: 200});
 
 function startGear(){
    TweenMax.to(bsGear, 2, {rotation:360, transformOrigin:"50% 50%", x: 100, ease: Power0.easeNone,  onComplete: startBook});
@@ -72,7 +70,7 @@ function startBook(){
 
 function startBook2(){
   var tl = new TimelineLite();
-  tl.to(bookTwo, 2, {rotation: 55,x:-1, transformOrigin:"100% 100%"});
+  tl.to(bookTwo, 2, {rotation: 55,x:-1, transformOrigin:"100% 100%",onComplete: bagLift});
 }
 
 // function startPin(){
@@ -80,7 +78,8 @@ function startBook2(){
 //   tl.from(bowlingPin, 2,{rotation:40, transformOrigin:"100% 100%", yoyo: false });
 // }
 function bagLift(){
-  TwweenLight.to(bag, 1, {y:-100})
+  TweenLite.to(bag, 1, {y:-440});
+  TweenLite.to(hook, 1, {y:-440, onComplete: jacksGame});
 }
 function jacksGame(){
     TweenLite.to(bball, 2, { fill:"red", ease: Bounce.easeOut, y: 100 });
